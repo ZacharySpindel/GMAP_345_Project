e61d6a457c8e02e3d87db7c8f0d4689111c38e8f
AresWong0 <123495391+AresWong0@users.noreply.github.com>
2024-11-23 23:40:55 -0500

    Added the sounds files

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound_Manager : MonoBehaviour
+{
+    public string sounds name;
+    public AudioClip clip;
+}
